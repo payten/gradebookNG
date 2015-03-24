0e824874c350d76120712893ac7a8065c8fef10f
Payten Giles <payten.giles@gmail.com>
2015-03-24 18:39:28 +1100

    WIP add placeholder AjaxCallListeners to the AjaxEditableLabels so we can hook into those events in our Javascript and show some notifications

--- tool/src/java/org/sakaiproject/gradebookng/tool/panels/GradeItemCellPanel.java
+++ tool/src/java/org/sakaiproject/gradebookng/tool/panels/GradeItemCellPanel.java
@@ -4,11 +4,11 @@ import org.apache.commons.lang.StringUtils;
 import org.apache.wicket.ajax.AjaxRequestTarget;
 import org.apache.wicket.ajax.attributes.AjaxRequestAttributes;
 import org.apache.wicket.extensions.ajax.markup.html.AjaxEditableLabel;
-import org.apache.wicket.markup.html.basic.Label;
-import org.apache.wicket.markup.html.link.ExternalLink;
+import org.apache.wicket.Component;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.Model;
 import org.apache.wicket.spring.injection.annot.SpringBean;
+import org.apache.wicket.ajax.attributes.AjaxCallListener;
 import org.sakaiproject.gradebookng.business.GradebookNgBusinessService;
 import org.sakaiproject.gradebookng.tool.model.GradeInfo;
 import org.sakaiproject.gradebookng.tool.model.StudentGradeInfo;
@@ -72,6 +72,28 @@ public class GradeItemCellPanel extends Panel {
 				Map<String,Object> extraParameters = attributes.getExtraParameters();
 				extraParameters.put("assignmentId", assignmentId);
 				extraParameters.put("studentUuid", studentGrades.getStudentUuid());
+
+        AjaxCallListener myAjaxCallListener = new AjaxCallListener() {
+          @Override
+          public CharSequence getBeforeSendHandler(Component component) {
+            return "console.log(\"updateLabelAjaxAttributes::I\'m executed before the firing of the Ajax call\")";
+          }
+
+          @Override
+          public CharSequence getSuccessHandler(Component component) {
+            return "console.log(\"updateLabelAjaxAttributes::I\'m executed on successful Ajax call\")";
+          }
+
+          @Override
+          public CharSequence getFailureHandler(Component component) {
+            return "console.log(\"updateLabelAjaxAttributes::I\'m executed on failed Ajax call\")";
+          }
+          @Override
+          public CharSequence getCompleteHandler(Component component) {
+            return "console.log(\"updateLabelAjaxAttributes::I\'m executed on completed Ajax call\")";
+          }
+        };
+        attributes.getAjaxCallListeners().add(myAjaxCallListener);
 			}
 			
 			@Override
@@ -80,9 +102,30 @@ public class GradeItemCellPanel extends Panel {
 				Map<String,Object> extraParameters = attributes.getExtraParameters();
 				extraParameters.put("assignmentId", assignmentId);
 				extraParameters.put("studentUuid", studentGrades.getStudentUuid());
+
+
+        AjaxCallListener myAjaxCallListener = new AjaxCallListener() {
+          @Override
+          public CharSequence getBeforeSendHandler(Component component) {
+            return "console.log(\"updateEditorAjaxAttributes::I\'m executed before the firing of the Ajax call\")";
+          }
+
+          @Override
+          public CharSequence getSuccessHandler(Component component) {
+            return "console.log(\"updateEditorAjaxAttributes::I\'m executed on successful Ajax call\")";
+          }
+
+          @Override
+          public CharSequence getFailureHandler(Component component) {
+            return "console.log(\"updateEditorAjaxAttributes::I\'m executed on failed Ajax call\")";
+          }
+          @Override
+          public CharSequence getCompleteHandler(Component component) {
+            return "console.log(\"updateEditorAjaxAttributes::I\'m executed on completed Ajax call\")";
+          }
+        };
+        attributes.getAjaxCallListeners().add(myAjaxCallListener);
 			}
-			
-			
 		};
 		
 		grade.setType(String.class);
